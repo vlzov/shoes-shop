8cc3a52c14a82cf9192806e1ee3501754e837f1d
vlzov <vlzov36@gmail.com>
2025-12-09 13:55:15 +0300

    Initial commit

---
+++ src/main/java/com/vsuet/shoeshop/ShoeshopApplication.java
@@ -0,0 +1,13 @@
+package com.vsuet.shoeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeshopApplication.class, args);
+	}
+
+}
